1f46f8811fc40fa035251cd3a47c4417b68624af
Marc Hensel <121319337+MarcOnTheMoon@users.noreply.github.com>
2024-11-26 07:13:28 +0100

    Moved debugging

--- c/src/lecture/05_Arrays/Debugging/Debugging.c
+++ c/src/lecture/05_Arrays/Debugging/Debugging.c
@@ -1,19 +1,31 @@
 /*****************************************************************************************************
  * Lecture sample code.
- * Sandbox to play around in during lecture.
+ * Demonstrate debugging with Visual Studio.
  *****************************************************************************************************
  * Author: Marc Hensel, http://www.haw-hamburg.de/marc-hensel
  * Project: https://github.com/MarcOnTheMoon/coding_learners_c/
- * Copyright: 2023, Marc Hensel
- * Version: 2023.08.23
+ * Copyright: 2024, Marc Hensel
+ * Version: 2024.11.26
  * License: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
  *****************************************************************************************************/
 
-#define _CRT_SECURE_NO_DEPRECATE	// Else MSVC++ prevents using scanf() (concern: buffer overflow)
 #include <stdio.h>
+#define SIZE 2
+
+int getSumAbs(const double a[SIZE]);
 
 int main(void)
 {
-	getchar();
-	return 0;
+    double a[] = { -2.7, 3.1, -4 },
+        double sumAbs = getSumAbs(a);
+    printf("Sum of absolute values: %.1f\n", sumAbs);
+    getchar();
+    return 0;
+}
+
+double getSumAbs(const double a[SIZE])
+{
+    double sum = 0.0;
+    for (int i = 1; i < SIZE; i++)
+        sum = abs(a[i]);
 }
